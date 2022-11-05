ad7c3d2fae6749691be880fa3737243a0a7137dc
Yana-Denisova <ladymacbeth@list.ru>
2022-11-05 11:50:04 +0300

    add flash massage

--- opinions_app.py
+++ opinions_app.py
@@ -1,10 +1,10 @@
-from datetime import datetime 
+# what_to_watch/opinions_app.py
 
+from datetime import datetime
 from random import randrange
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
 from wtforms.validators import DataRequired, Length, Optional
@@ -17,12 +17,14 @@ app.config['SECRET_KEY'] = 'MY SECRET KEY'
 
 db = SQLAlchemy(app)
 
+
 class Opinion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128), nullable=False)
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
 
 class OpinionForm(FlaskForm):
     title = StringField(
@@ -40,28 +42,39 @@ class OpinionForm(FlaskForm):
     )
     submit = SubmitField('Добавить')
 
+
 @app.route('/')
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
-        return 'В базе данных мнений о фильмах нет.'
+        return 'В базе данных записей нет.'
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
     return render_template('opinion.html', opinion=opinion)
+    
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_opinion_view():
-    # Вот тут создаётся новый экземпляр формы
-    form = OpinionForm()  
+    form = OpinionForm()
+    if form.validate_on_submit():
+        text = form.text.data
+        if Opinion.query.filter_by(text=text).first():
+            flash('Такое мнение уже было оставлено ранее!')
+            return render_template('add_opinion.html', form=form)
+        opinion = Opinion(
+            title=form.title.data,
+            text=form.text.data,
+            source=form.source.data
+        )
+        db.session.add(opinion)
+        db.session.commit()
+        return redirect(url_for('opinion_view', id=opinion.id))
     return render_template('add_opinion.html', form=form)
 
-# Тут указывается конвертер пути для id
+
 @app.route('/opinions/<int:id>')  
-# Параметром указывается имя переменной
 def opinion_view(id):  
-    # Теперь можно запрашивать мнение по id
     opinion = Opinion.query.get_or_404(id)  
-    # И передавать его в шаблон
     return render_template('opinion.html', opinion=opinion)
 
 if __name__ == '__main__':
